811e5384f69b60785c66d9973c9868064ec3cc4a
andriisheptun <andrijsheptun@gmail.com>
2025-06-14 12:02:46 +0300

    About Us finished

--- projects/estatein/assets/scripts/script.js
+++ projects/estatein/assets/scripts/script.js
@@ -59,5 +59,35 @@ let swiper = new Swiper(".mySwiper", {
       slidesPerView: 3,
       spaceBetween: 40,
     },
+    1920: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
+let swiper2 = new Swiper(".two-item-swiper", {
+  pagination: {
+    el: ".swipe-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swipe-button-next",
+    prevEl: ".swipe-button-prev",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1920: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
   },
 });
