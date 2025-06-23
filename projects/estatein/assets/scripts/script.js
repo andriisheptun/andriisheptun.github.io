6b695b22c8fc42769ad1710dc07401ae6f6f1e7c
andriisheptun <andrijsheptun@gmail.com>
2025-06-23 10:22:13 +0300

    FAQ`s slider dynamic content

--- projects/estatein/assets/scripts/script.js
+++ projects/estatein/assets/scripts/script.js
@@ -43,6 +43,44 @@ headerMenu.addEventListener("click", () => {
   headerMenu.classList.toggle("header-menu-hide");
 });
 
+async function fetchUrl(url) {
+  try {
+    let response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Failed to load: ' + response.status);
+    }
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+async function renderFAQCards() {
+  let data = await fetchUrl('assets/scripts/estatein_faq.json');
+
+  let wrapper = document.getElementById('faq').querySelector('.swiper-wrapper');
+
+  data.forEach(item => {
+    let slide = document.createElement('div');
+
+    slide.classList.add('swiper-slide', 'swiper3-slide');
+    slide.innerHTML = `
+      <h3>${item.question}</h3>
+      <p class="obscured-text">${item.description}</p>
+      <a href="#" class="btn btn-fill__black">Read More</a>  
+  `;
+
+    wrapper.appendChild(slide);
+  });
+}
+
+
+
+
+
+
 document.querySelectorAll('#standartSwipers .mySwiper').forEach((swiperEl) => {
   const paginationEl = swiperEl.querySelector('.swipe-pagination');
   const nextBtn = swiperEl.querySelector('.swipe-button-next');
@@ -190,3 +228,6 @@ let mainSwiper = new Swiper(mainSwiperEl, {
     },
   },
 });
+
+
+renderFAQCards();
